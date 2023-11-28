83d3856ec1861359fe2d768e3ebfed3bbaa97b43
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-11-28 04:43:38 -0800

    Leap of faith

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for word in range(ord('a'), ord('z') + 1):
-    if((word == ord('q')) or (word == ord('e'))):
+    if ((word == ord('q')) or (word == ord('e'))):
         continue
     print('{:c}'.format(word), end="")
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for num in range(99):
+    print(f"{num:02d}", end=", ")
+print(f"{99:d}")
