837c60134f78d0120427007f888268c6a94821d4
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-11-28 05:28:08 -0800

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for word in str:
+        asc = ord(word)
+        if (asc > 96) and (asc < 123):
+            asc = asc - 32
+        print('{:c}'.format(asc), end='')
+    print('{:c}'.format(10))
