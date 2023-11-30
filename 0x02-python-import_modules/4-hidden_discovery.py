23a38b2ee669a2fa480220f30b3450d90454d387
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-11-30 04:52:56 -0800

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    print(dir(hidden_4))
+    lilac = dir(hidden_4)
+    for word in lilac:
+        if word[0] == '_' and word[1] == '_':
+            continue
+        print(word)
