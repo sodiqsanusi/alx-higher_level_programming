e41c9e917753dd15427fbfadd1c2aa093d3b9ee0
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-12-03 07:55:25 -0800

    Task 3 bug??

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return
     idx = len(my_list)
     while idx > 0:
         print("{:d}".format(my_list[idx - 1]))
