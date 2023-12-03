4358ac2f777702dfcf12a784186abd3fd1266023
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-12-03 13:36:33 -0800

    Task 11 bug, trial 1

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if my_list is None:
         return
-    if idx >= len(my_list):
+    if idx >= len(my_list) or idx < 0:
         return (my_list)
     del my_list[idx]
     return (my_list)
