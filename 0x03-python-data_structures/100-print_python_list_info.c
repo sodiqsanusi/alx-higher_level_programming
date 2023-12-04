c0707a01c5ccf3f6bbde9c9c000e2dd13d8280bf
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-12-04 04:42:37 -0800

    Wanted to try, gave up

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,10 @@
+/**
+ * print_python_list_info - Prints an ifo about Python lists
+ * @p: "PyObject *" Don't really know what it does lol
+ *
+ * Return: "void" Nothing
+ */
+void print_python_list_info(PyObject *p)
+{
+}
+
