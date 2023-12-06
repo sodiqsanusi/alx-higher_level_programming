ea90a37e31dfc9a187bd310a74bc2af40b04fc74
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-12-06 07:46:44 -0800

    Task 10 bug, trial one 1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     final = None
+    if a_dictionary is None or a_dictionary == {}:
+        return (final)
     for key, value in a_dictionary.items():
         if final is None:
             final = key
