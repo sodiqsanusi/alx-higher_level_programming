6faddfc8711d4f204cdbc1a9dce2e5fe69ec78c3
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-12-18 16:13:53 -0800

    Task 2 bug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,7 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     lilac = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             lilac += 1
         except (TypeError, ValueError):
             i += 1
