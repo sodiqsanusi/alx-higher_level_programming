abbb6a6c9546670b601eb66b4a2d42371a057e48
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-12-19 16:28:15 -0800

    Task 4: Access and update private attributes using getter and setter methods

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for creating the Square class alongside setter, getter methods"""
+
+
+class Square:
+    """This is the definition of the class"""
+    def __init__(self, size=0):
+        """init method for all instances of the class"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter method for the private size attribute"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter method for the size private attribute"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returns the area of the square"""
+        return (self.__size ** 2)
