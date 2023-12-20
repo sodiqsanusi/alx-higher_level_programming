b9167bcb4c34017ba231cb804a7f0a5169c5bb7e
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2023-12-19 16:02:39 -0800

    Task 1: Create a class with a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Creating a class Square with a private attribute
+"""
+
+
+class Square:
+    """This is the definition of the class"""
+    def __init__(self, size):
+        """ The init method for the class"""
+        self.__size = size
