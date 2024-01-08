e87190e352ed38ddcc88c5145fb8076fc5cbbed2
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-08 14:58:44 -0800

    Task 6: Add a public instance method that raises an exception to Task 5

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Contains a minimal class"""
+
+
+class BaseGeometry():
+    """This is an empty class, or is it?"""
+    def area(self):
+        """Main function is to raise an exception"""
+        raise Exception("area() is not implemented")
