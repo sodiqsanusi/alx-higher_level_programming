0d518eb519eab81484b1abf52a54d274f377a46a
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-08 14:41:31 -0800

    Task 2 bug: linting error

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""This module contains a function that checks if an object is a sublsubclass"""
+"""This module contains a function that checks if an object is a subclass"""
 
 
 def is_same_class(obj, a_class):
