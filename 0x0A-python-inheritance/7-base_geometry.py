76c812da8d31c6d201d8cff7acd1fd96d68ca9bc
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-08 15:30:22 -0800

    Task 7: A class that has some validation errors

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Contains a minimal class with two public methods"""
+
+
+class BaseGeometry():
+    """This class contains some functions that builds on mathematics"""
+    def area(self):
+        """Main function is to raise an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """A function that validates a value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
