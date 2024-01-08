628d31ccb7be5839c7f01833a71cbdffb50f7cba
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-08 14:40:18 -0800

    Task 2: Check if object is an exact instance of a class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""This module contains a function that checks if an object is a sublsubclass"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if a specified object is a subcclass of a specified class"""
+    return (type(obj) is a_class)
