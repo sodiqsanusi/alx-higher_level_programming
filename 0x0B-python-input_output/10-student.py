554bf196fe0bc2fdb7ed4507c45d6f00151c8da7
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-09 15:00:52 -0800

    Task 10: Getting drowsy and burnttt out

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""This module contains a class definition"""
+
+
+class Student:
+    """Defines a Student object blueprint"""
+    def __init__(self, first_name, last_name, age):
+        """Instatiation method for the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dict representatino of the class instance"""
+        if type(attrs) is not list:
+            return (self.__dict__)
+        lilac = {}
+        for attr in attrs:
+            if attr in self.__dict__.keys():
+                lilac[attr] = self.__dict__[attr]
+        return (lilac)
