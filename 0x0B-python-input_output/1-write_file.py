a59ca750c1bb5ae80ccec39c7c0349a0c5b3bf84
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-09 12:00:08 -0800

    Task 1 bug: Lack of adequate documentation

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,16 @@
 
 
 def write_file(filename="", text=""):
+    """
+    Writes to a file, overwrite existing content or creates new file
+
+    Args:
+        filename (str): Name of file to create/overwrite
+        text (str): Text content to put into the newly created file
+
+    Return:
+        (int) The number of characters written into the file
+    """
     with open(filename, "w", encoding="utf-8") as createdFile:
         lilac = createdFile.write(text)
         return (lilac)
