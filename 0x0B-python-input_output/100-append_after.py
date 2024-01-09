f9fd5e3048502e3c811d9c99dbf47941fbb07282
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-09 15:35:30 -0800

    Task 13 (Advanced): Insert a line of text to a file, after a specific string

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""This module contains some file manipulation"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file, after each line containing a
+    specific string
+
+    Args:
+        filename (str): Filename of text file to read an append to
+        search_string (str): String to be used as search substring
+        new_string (str): The string to be appended after the search_string
+    """
+    with open(filename, "w+", encoding="utf-8") as openedFile:
+        textData = openedFile.readlines()
+        lilac = []
+        for line in textData:
+            lilac.append(line)
+            if (line.find(search_string) >= 0):
+                lilac.append(new_string)
+        openedFile.writelines(lilac)
