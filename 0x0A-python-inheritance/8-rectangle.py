3a168a4b090635b8e514a4e87b602743c61d8b34
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-08 16:35:30 -0800

    Task 8 bug: closing in

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -33,7 +33,9 @@ class Rectangle(BaseGeometry):
     """A subclass of the base geometry shape"""
 
     def __init__(self, width, height):
+        """The instanstiation method of the subclass"""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+
         self.__width = width
         self.__height = height
