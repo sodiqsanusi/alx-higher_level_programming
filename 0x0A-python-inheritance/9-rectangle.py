db079e74fc364958cc8b6311ac6624fd93202fb2
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-08 16:43:44 -0800

    Task 9: Create a method that overrides the parent class method

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+"""A module with a class that models geometric shapes."""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A subclass of the base geometry shape"""
+
+    def __init__(self, width, height):
+        """The instanstiation method of the subclass"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """An overridden function that calculates area"""
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """A magic method that runs when str or print is called"""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
