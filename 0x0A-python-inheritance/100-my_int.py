486f1c35a71091d578ce4edc052ca2ff82fa1f7e
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-08 17:14:03 -0800

    Task 12 (Advanced): Trying to get my average a bit higher

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""This module builds on the int standard class"""
+
+
+class MyInt(int):
+    """This is a custom modification of the int class"""
+    def __eq__(self, __value):
+        """Dunder method for the == operator"""
+        return (super().__ne__(__value))
+
+    def __new__(self, __value):
+        """Dunder method for the != operator"""
+        return (super().__eq__(__value))
