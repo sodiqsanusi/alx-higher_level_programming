c67e9997a57d1445aa3dcea63f1d2bc6d3cc4778
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-09 11:27:43 -0800

    Task 0: Read a file and print to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This module reads a text file and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """Opens a text file encoded in utf-8, prints to stdout"""
+    with open(filename, "r", encoding="utf-8") as openedFile:
+        lilac = openedFile.read()
+        print(lilac)
