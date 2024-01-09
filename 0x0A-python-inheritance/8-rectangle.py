16de1892e7fcc682b5c4dcd927ef16cfd32eda49
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-08 16:38:58 -0800

    Task 8 bug: Or an import error?

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,30 +4,7 @@
 """A module with a class that models geometric shapes."""
 
 
-class BaseGeometry:
-    """A base class for geometry shapes."""
-
-    def area(self):
-        """Returns the area of a shape"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Performs validation of integer values.
-
-        Args:
-            name (str): The name of the object.
-            value (int): The value of the object.
-
-        Raises:
-            TypeError: When the `value` given to the object is not an integer.
-            ValueError: When the the `value` is less than or equal to zero.
-        """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
