4b9bde7abea2390721ec8881439ac77a7627e213
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-09 16:04:34 -0800

    Task 13 bug: Logical error from my side, totally forgot about the file pointer

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -13,11 +13,13 @@ def append_after(filename="", search_string="", new_string=""):
         search_string (str): String to be used as search substring
         new_string (str): The string to be appended after the search_string
     """
-    with open(filename, "w+", encoding="utf-8") as openedFile:
+    with open(filename, "r", encoding="utf-8") as openedFile:
         textData = openedFile.readlines()
-        lilac = []
-        for line in textData:
-            lilac.append(line)
-            if (line.find(search_string) >= 0):
-                lilac.append(new_string)
-        openedFile.writelines(lilac)
+    lilac = []
+    for line in textData:
+        lilac.append(line)
+        if (line.find(search_string) >= 0):
+            lilac.append(new_string)
+
+    with open(filename, "w", encoding="utf-8") as newFile:
+        newFile.writelines(lilac)
