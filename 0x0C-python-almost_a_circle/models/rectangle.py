551327e89c0116429486d65875f34c7202c9adfc
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-14 01:18:04 -0800

    Task 3: Validating attributes from setter methods

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -28,6 +28,10 @@ class Rectangle(Base):
         """
         Setter method for the width private attribute
         """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -42,6 +46,10 @@ class Rectangle(Base):
         """
         Setter method for the height private attribute
         """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -56,6 +64,10 @@ class Rectangle(Base):
         """
         Setter method for the private attribute x
         """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -70,4 +82,8 @@ class Rectangle(Base):
         """
         Setter method for the private attribute y
         """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
