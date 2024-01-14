aede2bdcf594186eb7d242e0f94be15fd4c7e0df
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-01-14 04:12:45 -0800

    Task 11: Add a setter/getter method to the Square subclass for zise

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,3 +21,22 @@ class Square(Rectangle):
             f"[{type(self).__name__}] ({self.id}) "
             f"{self.x}/{self.y} - {self.width}"
         )
+
+    @property
+    def size(self):
+        """
+        Getter method for the size of the Square instance
+        """
+        return (self.width)
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method for the size of the Square instance
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
