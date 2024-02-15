d844fddc46cf050ef77153c44e7b765ae8b2c948
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-02-14 17:17:59 -0800

    Initial commit, binging

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- Shows all privileges of a server user in localhost
+SHOW GRANTS FOR "user_0d_1"@"localhost";
+SHOW GRANTS FOR "user_0d_2"@"localhost";
---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- Create a user, and a database
+CREATE USER IF NOT EXISTS "user_0d_2"@"localhost" IDENTIFIED BY "user_0d_2_pwd";
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON hbtn_0d_2.* TO "user_0d_2"@"localhost";
