af486fe5da15c11106fe2ac944e891cf62820d3b
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-02-14 17:54:26 -0800

    Keeping it up in the clous=ds

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- JOIN two tables in a databse with the LEFT JOIN
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
