c233adf7395ea0fc55f330ba540e1adb291222e9
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-03-12 20:42:34 +0000

    Task 6: Inherit and overturn a parent method

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
