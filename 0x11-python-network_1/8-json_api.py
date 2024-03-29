4cade9a554c163b666ded543c9017ec7ab4767cc
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-03-28 18:17:21 -0700

    Task 8: Send an inputted letter to a URL, handling error and JSON formatting on response

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Take in a latter, and send as a POST request. display gotten result as JSON
+"""
+import requests
+import sys
+
+
+def main():
+    """
+    This is the main function for the program
+    """
+    url = "http://7fcf1e331834.3e24de83.alx-cod.online:5000/search_user"
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    data = {"q": letter}
+    res = requests.post(url, data)
+    try:
+        res.raise_for_status()
+        json_res = res.json()
+        if len(json_res) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_res['id'], json_res['name']))
+    except Exception:
+        print("Not a valid JSON")
+
+
+if __name__ == "__main__":
+    main()
