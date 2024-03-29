de08a24419688d244220df712830304d8b31524e
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-03-28 18:25:27 -0700

    Task 10: Use requests to send an authorized GET to Github API

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Uses the GitHub API to display user ID.
+Authentication is necessary
+"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    res = requests.get("https://api.github.com/user", auth)
+    print(res.json().get("id"))
