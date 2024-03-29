2d74b3a0d1163a6b29e265c49e6cbfcd8fd9e7aa
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-03-28 18:27:41 -0700

    Task 10 bug: trying to see what exactly I did wring

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,6 +9,6 @@ import sys
 
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    res = requests.get("https://api.github.com/user", auth)
+    auth_key = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    res = requests.get("https://api.github.com/user", auth=auth_key)
     print(res.json().get("id"))
