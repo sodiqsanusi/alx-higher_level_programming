b74e059c64e798b5e4d86b1ed40ee6c544909735
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-04-23 16:06:02 -0700

    Task 4: Use the results of a requests to infer solutions to a different problem

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+/*
+* Use data from a GET request to make other conclusions
+*/
+
+const request = require('request');
+const url = process.argv[2];
+let finalResponse;
+
+const helper = (arr, url) => {
+  let count = 0;
+  for (const result of arr) {
+    const characters = result.characters;
+    const test = characters.some((x) => x === url);
+    if (test) {
+      count++;
+    }
+  }
+  return (count);
+};
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    process.exitCode = 1;
+  }
+  finalResponse = JSON.parse(body);
+  const person = 'https://swapi-api.alx-tools.com/api/people/18/';
+  finalResponse = helper(finalResponse.results, person);
+  console.log(finalResponse);
+});
