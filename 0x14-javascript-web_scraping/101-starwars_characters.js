40923d5c681e801d2d2ec0e182ec438eea59adea
Sodiq "Ade" Sanusi <ademilddiq@gmail.com>
2024-04-23 17:30:07 -0700

    Task 100 (advanced): Use result gotten from a request to make further requests to get a final solution

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+/*
+* From the result gotten from a GET request, make other requests
+*/
+
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    process.exitCode = 1;
+    return;
+  }
+  const data = JSON.parse(body).characters;
+  for (const character of data) {
+    request(character, function (error, response, body) {
+      if (error) {
+        console.error(error);
+        process.exitCode = 1;
+        return;
+      }
+      const name = JSON.parse(body).name;
+      console.log(name);
+    })
+  }
+});
